883b8656a597a8880b5ce552237539a85b495557
t43g3ntl3man <alitaimur4@gmail.com>
2022-09-02 06:57:14 +0500

    firedUP

---
+++ locfinder.php
@@ -0,0 +1,11 @@
+<?php
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+else
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+echo $ip_address;
+?>
